d7a5bf23b55d3616b9a61ef0056516e36beb1ff0
root <root@linux.(none)>
2014-02-10 18:15:23 -0800

    pager.h

---
+++ pager.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <signal.h>
+
+
+#define PAGER_INIT        \
+    int pgp__ = 0;        \
+    void pgk__(int s) {   \
+    if (pgp__)            \
+    close(1);             \
+    }
+
+
+#define BEGIN_PAGER       \
+signal(SIGCHLD,pgk__);    \
+    int pager__;          \
+    int pipes__[2];       \
+    pipe(pipes__);        \
+    pgp__ = 1;            \
+    pager__ = fork();     \
+    if (pager__==0) {     \
+    dup2(pipes__[0], 0);  \
+    close(pipes__[1]);    \
+    char *args__[]=       \
+    {"more",0};           \
+    execvp(args__[0],     \
+    args__);              \
+    } else {              \
+    int stdout__=dup(1);  \
+    dup2(pipes__[1], 1);          
+ 
+ 
+#define END_PAGER         \
+    fflush(stdout);       \
+    close(pipes__[0]);    \
+    close(pipes__[1]);    \
+    close(1);             \
+    wait(&pager__);       \
+    pgp__ = 0;            \
+    dup2(stdout__, 1);    \
+    }
+ 
